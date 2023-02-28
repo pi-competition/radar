39e81540d4f95adbda1cead044e94e9526e153a6
Ben <50147798+Ben754444@users.noreply.github.com>
2023-02-28 17:37:40 +0000

    return some actual json this time

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Response
 import json
 import main
 
@@ -7,8 +7,7 @@ app = Flask(__name__)
 @app.route('/devices', methods=['GET'])
 def devices():
     print(main.last_run)
-    return json.dumps(
-        {
+    return Response(json.dumps({
             "success": True,
             "status": 200,
             "data": {
@@ -16,41 +15,32 @@ def devices():
                 "last_run": main.last_run,
                 "last_ping": main.last_ping
             }
-        })
+
+    }), mimetype='application/json')
 
 @app.errorhandler(404)
 def not_found(error):
-    return json.dumps(
-        {
+    return Response(json.dumps({
             "success": False,
             "status": 404,
             "error": "NOT_FOUND"
-
-        }
-    ), 404
+    }), mimetype='application/json', status=404)
 
 @app.errorhandler(405)
 def method_not_allowed(error):
-    return json.dumps(
-        {
+    return Response(json.dumps({
             "success": False,
             "status": 405,
             "error": "METHOD_NOT_ALLOWED"
-
-        }
-    ), 405
+            }), mimetype='application/json', status=405)
 
 @app.errorhandler(500)
 def internal_server_error(error):
     print(error)
-    return json.dumps(
-        {
+    return Response(json.dumps({
             "success": False,
             "status": 500,
             "error": "INTERNAL_SERVER_ERROR"
-
-        }
-    ), 500
-
+            }), mimetype='application/json', status=500)
 
 app.run(host='127.0.0.1', port=5000)
